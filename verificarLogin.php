ea4b057c9ea04f6c2ad131f75e5cca735ad4c392
Angel Solis <Angel Solis@AngelSolis-PC>
2013-12-22 23:02:13 -0600

    login funcional

--- verificarLogin.php
+++ verificarLogin.php
@@ -9,7 +9,10 @@ $usuario->setUsuario($_GET["usua"]);
 $usuario->setPass($dao->genera_md5($_GET["pass"]));
 $existe=$dao->validarLogin($usuario);
 if($existe == true){
-    header('Location: index.php');
+    echo " <script>
+             document.location.href='index.php';
+        </script>";
+//    header('Location: index.php');
 }else{
     echo "<script> alertify.error('mal')</script>";
 }
